1c599bb0474a0e97d964c56d605b050f9b48e6a3
lvguoqiang <guoqiang.lv@hand-china.com>
2018-04-08 20:14:22 +0800

    配置swagger

--- src/main/java/com/hand/exercise/web/UserController.java
+++ src/main/java/com/hand/exercise/web/UserController.java
@@ -2,6 +2,7 @@ package com.hand.exercise.web;
 
 import com.hand.exercise.domain.User;
 import com.hand.exercise.service.UserService;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2018/3/28
  */
 @RestController
+@Api(description = "用户基本信息api")
 public class UserController {
 
     @Autowired
